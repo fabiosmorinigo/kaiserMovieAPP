d9d5afd3de955abb7908df8fa5dea9c961d7e80d
fabiosmorinigo <fabio_sosapy@outlook.com>
2022-07-23 17:21:49 -0400

    add the function categoryPreview

--- src/main.js
+++ src/main.js
@@ -23,26 +23,27 @@ async function getTrendingMoviesPreview() {
 
 
 async function getCategoryPreview() {
-    const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY);
+    const res = await fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=' + API_KEY);
     const data = await res.json();
 
-    const movies = data.results;
-    movies.forEach(movie => {
-        const trendingPreviewMoviesContainer = document.querySelector('#trendingPreview .trendingPreview-movieList');
+    const categories = data.genres;
+    categories.forEach(category => {
 
-        const movieContainer = document.createElement('div');
-        movieContainer.classList.add('movie-container');
+        const previewCategoryContainer = document.querySelector('#categoriesPreview .categoriesPreview-list');
 
-        const movieIMG = document.createElement('img');
-        movieIMG.classList.add('movie-img');
-        movieIMG.setAttribute('alt', movie.title);
-        movieIMG.setAttribute(
-            'src',
-            'https://image.tmdb.org/t/p/w300' + movie.poster_path);
-        
-        movieContainer.appendChild(movieIMG);
-        trendingPreviewMoviesContainer.appendChild(movieContainer)
+        const categoryContainer = document.createElement('div');
+        categoryContainer.classList.add('category-container');
+
+
+        const categoryTitle = document.createElement('h3');
+        categoryTitle.textContent = category.name;
+        categoryTitle.classList.add('category-title')
+        categoryTitle.setAttribute('id', 'id' + category.id);
+
+        categoryContainer.appendChild(categoryTitle);
+        previewCategoryContainer.appendChild(categoryContainer);
     });
 }
 
 getTrendingMoviesPreview();
+getCategoryPreview();
